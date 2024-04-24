ef5bedfb920c625853ca0a985fe4226a2ecaa0e1
lynstorm <ala.ouhichi99@gmail.com>
2024-04-24 15:10:45 +0100

    signupV

--- src/Entity/Vendeur.php
+++ src/Entity/Vendeur.php
@@ -14,6 +14,7 @@ class Vendeur implements UserInterface
     #[ORM\Column(name: "IdVendeur", type: "integer", nullable: false)]
    private ?int $idvendeur =  null;
 
+   
    #[Assert\NotBlank(message: "Nom Required")]
    #[ORM\Column(name: "nom", type: "string", length: 255, nullable: false)]
    private ?string $nom = null;
